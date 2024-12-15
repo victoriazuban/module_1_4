304e7ab4496e9b7e97abf60badc735771cdd4441
victoriazuban <victoriazyuban@icloud.com>
2024-12-16 00:15:30 +0200

    Add files via upload

---
+++ module_1_4main.py
@@ -0,0 +1,9 @@
+
+#module_1_4.ру
+my_string = input('Как твои дела?')
+print(len(my_string))
+print('my_string'.upper())
+print('MY_STRING'.lower())
+print('my_string'[0])
+print('my_string'[-1])
+print('my string'.replace(' ',''))
